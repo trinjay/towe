1cff433e4f9cf34a9258270b790d2a628077d480
trinjay <75678794+trinjay@users.noreply.github.com>
2021-08-23 13:36:52 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,32 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 20
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            strokeWeight(4);
+           fill("red");
+           line(pointA.x,pointA.y, pointB.x,pointB.y);
+
+           // Write line() to draw a line from pointA to pointB
+
+        }
+    }
+    
+}
